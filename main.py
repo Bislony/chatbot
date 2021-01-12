5ea07d2b6c03a091ba2efde1ca51fb781bf3dd10
Bislony <liyavolman45@gmail.com>
2021-01-12 14:08:58 +0200

    הוספתי אפשרויות בחירה ותשובה ריגשית הזדהות

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ print('Loony: Hello !!!')
 name = input('Loony: What is your name?\n - ')
 print('Loony: Hello ' + name + '!!')
 print('Loony: It is nice to meet you ' + name + '!!!')
-print('Loony: How are you ' + name + '??')
-input(name + ': ')
+print('Loony: How are you ' + name + '?? (good/not good)')
+answer = input(name + ': ')
+if 'good' == answer:
+    print('I am glad to hear!!')
+else:
+    print('I am sad😫')
